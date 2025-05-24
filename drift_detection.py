0c6253a25977902ac3ced95a44cce6f542a36372
percy0118 <135147934+percy0118@users.noreply.github.com>
2025-05-24 10:48:06 +0530

    Add files via upload

---
+++ drift_detection.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+from sklearn import datasets
+
+from evidently.test_suite import TestSuite
+from evidently.test_preset import DataStabilityTestPreset
+
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv('data/data.csv')
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+reference_data = pd.read_csv('reference_data.csv')
+new_data = pd.read_csv('new_data.csv')
+
+import numpy as np
+
+
+def introduce_drift(data, drift_features, drift_amount=0.1, random_seed=42):
+    np.random.seed(random_seed)
+    drifted_data = data.copy()
+    
+    for feature in drift_features:
+        if feature in data.columns:
+            drifted_data[feature] += np.random.normal(loc=0, scale=drift_amount, size=data.shape[0])
+    
+    return drifted_data
+    
+features_to_drift = ['Glucose', 'BloodPressure', 'SkinThickness', 'Pregnancies']
+
+drifted_data = introduce_drift(X_test, features_to_drift, drift_amount=50)
+
+reference_data['Outcome'] = y_train.reset_index(drop = True)
+drifted_data['Outcome'] = y_test.reset_index(drop = True)
+
+drifted_data.to_csv('new_data.csv', index=False)
+reference_data.to_csv('reference_data.csv', index=False)
+
+## Combine data drift and the reference data
+
+pd.concat([reference_data, drifted_data]).reset_index(drop =True).to_csv('combined_data.csv', index=False)
+data_drift_report = Report(metrics=[
+    DataDriftPreset(),
+])
+
+data_drift_report.run(current_data=drifted_data, reference_data=reference_data, column_mapping=None)
+
+
+
+import datetime
+
+today_date = datetime.datetime.today().strftime("%Y-%m-%d")
+data_drift_report.save_html('reports\data_drift_report_{today_date}.html')
+
+from evidently.metric_preset import DataDriftPreset
+from evidently.report import Report
+
+data_drift_report = Report(metrics=[
+    DataDriftPreset(),
+])
+
+data_drift_report.run(current_data=drifted_data.drop('Outcome', axis =1), reference_data=reference_data.drop('Outcome', axis =1), column_mapping=None)
+report_json = data_drift_report.as_dict()
+drift_detected = report_json['metrics'][0]['result']['dataset_drift']
+
+print(drift_detected)
---
+++ retraining.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import pickle
+
+df = pd.read_csv('data/data.csv')
+X = df.drop('Outcome', axis=1)
+y = df['Outcome']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+#X = df.drop('Outcome', axis=1)
+#y = df['Outcome']
+
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+numeric_features = X.columns
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features)
+    ])
+	
+	
+from sklearn.ensemble import RandomForestClassifier
+
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', RandomForestClassifier(random_state=42))
+])
+
+pipeline.fit(X_train, y_train)
+
+# create an iterator object with write permission - model.pkl
+with open('model_pkl', 'wb') as files:
+    pickle.dump(pipeline, files)
+
+#'wb') as f:
+#   pickle.dump(pipeline, f)
+    
+#saved_model = pickle.dumps(pipeline,f)
